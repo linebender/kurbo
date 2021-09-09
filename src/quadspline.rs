64cc09a31ae8bc22054e9d9d36555490dab63599
Simon Cozens <simon@simon-cozens.org>
2021-09-09 20:35:37 +0100

    Apply doc fix suggestions from code review
    Co-authored-by: Colin Rofls <colin@cmyr.net>

--- src/quadspline.rs
+++ src/quadspline.rs
@@ -2,17 +2,17 @@
 use crate::Point;
 
 #[derive(Clone, Debug, PartialEq)]
-#[allow(missing_docs)]
+/// A quadratic Bézier spline.
 pub struct QuadSpline(Vec<Point>);
 
 impl QuadSpline {
-    /// Construct a new quadratic spline from an array of points.
+    /// Construct a new `QuadSpline` from an array of [`Point`]s.
     #[inline]
     pub fn new(points: Vec<Point>) -> Self {
         Self(points)
     }
 
-    /// Return the control points of the spline.
+    /// Return the spline's control [`Point`]s.
     #[inline]
     pub fn points(&self) -> &[Point] {
         &self.0
