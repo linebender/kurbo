55fb27d027c00371e354cd7f4a577194758367fe
Raph Levien <raph@google.com>
2022-09-29 09:04:29 -0700

    Additional experiments with moments

--- examples/euler2.rs
+++ examples/euler2.rs
@@ -1,21 +1,44 @@
-use kurbo::{CubicBez, EulerSeg, ParamCurve, ParamCurveArea, ParamCurveDeriv, common::GAUSS_LEGENDRE_COEFFS_32, Point};
+use kurbo::{
+    common::GAUSS_LEGENDRE_COEFFS_32, CubicBez, EulerSeg, ParamCurve, ParamCurveArea,
+    ParamCurveDeriv, Point,
+};
 use rand::{thread_rng, Rng};
 
 fn calc_euler_area(e: &EulerSeg) -> f64 {
     let d = e.deriv();
-    GAUSS_LEGENDRE_COEFFS_32.iter().map(|(wi, xi)| {
-        let t = 0.5 + 0.5 * xi;
-        (0.5 * wi) * e.eval(t).y * d.eval(t).x
-    }).sum::<f64>()
+    GAUSS_LEGENDRE_COEFFS_32
+        .iter()
+        .map(|(wi, xi)| {
+            let t = 0.5 + 0.5 * xi;
+            (0.5 * wi) * e.eval(t).y * d.eval(t).x
+        })
+        .sum::<f64>()
 }
 
 fn calc_mx<T: ParamCurve + ParamCurveDeriv>(e: &T) -> f64 {
     let d = e.deriv();
-    GAUSS_LEGENDRE_COEFFS_32.iter().map(|(wi, xi)| {
-        let t = 0.5 + 0.5 * xi;
-        let p = e.eval(t);
-        (0.5 * wi) * (p.x - 0.5) * p.y * d.eval(t).x
-    }).sum::<f64>()
+    GAUSS_LEGENDRE_COEFFS_32
+        .iter()
+        .map(|(wi, xi)| {
+            let t = 0.5 + 0.5 * xi;
+            let p = e.eval(t);
+            (0.5 * wi) * (p.x - 0.5) * p.y * d.eval(t).x
+        })
+        .sum::<f64>()
+}
+
+fn calc_mx2<T: ParamCurve + ParamCurveDeriv>(e: &T) -> f64 {
+    let d = e.deriv();
+    GAUSS_LEGENDRE_COEFFS_32
+        .iter()
+        .map(|(wi, xi)| {
+            let t = 0.5 + 0.5 * xi;
+            let p = e.eval(t);
+            let x = 2.0 * p.x - 1.0;
+            (0.5 * wi) * (2.0 * x * x - 1.0) * p.y * d.eval(t).x
+            //(0.5 * wi) * (8.0 * x * x  * x - 4.0 * x) * p.y * d.eval(t).x
+        })
+        .sum::<f64>()
 }
 
 fn rand_shallow_cubic() -> CubicBez {
@@ -30,16 +53,22 @@ fn main() {
     for _ in 0..100_000 {
         let c = rand_shallow_cubic();
         let e = EulerSeg::from_cubic(c);
-        let err  = e.cubic_euler_err(c, 10);
+        let err = e.cubic_euler_err(c, 10);
         let area_err = calc_euler_area(&e) + c.signed_area();
         let mx_err = calc_mx(&e) - calc_mx(&c);
+        let mx2_err = calc_mx2(&e) - calc_mx2(&c);
         let est_err = (area_err / 0.82).hypot((mx_err * 5.0).abs());
-        let crudest_err = (c.p1 - Point::new(1./3., 0.)).hypot2() + (c.p2 - Point::new(2./3., 0.)).hypot2();
-        let est_err = est_err + 0.03 * crudest_err.powf(1.5);
+        let even_err = area_err.hypot(mx_err * 4.0);
+        let crudest_err =
+            (c.p1 - Point::new(1. / 3., 0.)).hypot2() + (c.p2 - Point::new(2. / 3., 0.)).hypot2();
+        //let est_err = est_err + 0.03 * crudest_err.powf(1.5);
         //let est_err = est_err + 0.6 * crudest_err.powf(2.0);
-        if est_err < 0.001 {
-            println!("{} {}", err, est_err);
+        let lc = (c.p3 - c.p0).hypot();
+        let lp = (c.p1 - c.p0).hypot() + (c.p2 - c.p1).hypot() + (c.p3 - c.p2).hypot();
+        let pc_err = lp - lc;
+
+        if err > 0.001 && err < 0.0011 {
+            println!("{} {}", crudest_err, pc_err);
         }
     }
 }
-
