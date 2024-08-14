linebender/kurbo

commit 8d10d4e00981238f1ba555fe44af000b81aa4ec1

[clippy] Push the rock back up the hill

src/svg.rs
@@ -280,9 +280,9 @@ impl<'a> SvgLexer<'a> {
     fn get_cmd(&mut self, last_cmd: u8) -> Option<u8> {
         self.skip_ws();
         if let Some(c) = self.get_byte() {
-            if (b'a'..=b'z').contains(&c) || (b'A'..=b'Z').contains(&c) {
+            if c.is_ascii_lowercase() || c.is_ascii_uppercase() {
                 return Some(c);
-            } else if last_cmd != 0 && (c == b'-' || c == b'.' || (b'0'..=b'9').contains(&c)) {
+            } else if last_cmd != 0 && (c == b'-' || c == b'.' || c.is_ascii_digit()) {
                 // Plausible number start
                 self.unget();
                 return Some(last_cmd);
@@ -314,7 +314,7 @@ impl<'a> SvgLexer<'a> {
         let mut digit_count = 0;
         let mut seen_period = false;
         while let Some(c) = self.get_byte() {
-            if (b'0'..=b'9').contains(&c) {
+            if c.is_ascii_digit() {
                 digit_count += 1;
             } else if c == b'.' && !seen_period {
                 seen_period = true;
@@ -329,11 +329,11 @@ impl<'a> SvgLexer<'a> {
                 if c == b'-' || c == b'+' {
                     c = self.get_byte().ok_or(SvgParseError::Wrong)?
                 }
-                if !(b'0'..=b'9').contains(&c) {
+                if c.is_ascii_digit() {
                     return Err(SvgParseError::Wrong);
                 }
                 while let Some(c) = self.get_byte() {
-                    if !(b'0'..=b'9').contains(&c) {
+                    if c.is_ascii_digit() {
                         self.unget();
                         break;
                     }
@@ -370,7 +370,7 @@ impl<'a> SvgLexer<'a> {
 
     fn get_maybe_relative(&mut self, cmd: u8) -> Result<Point, SvgParseError> {
         let pt = self.get_number_pair()?;
-        if (b'a'..=b'z').contains(&cmd) {
+        if cmd.is_ascii_lowercase() {
             Ok(self.last_pt + pt.to_vec2())
         } else {
             Ok(pt)

commit 1cd126bbc567a8d047d5b009f21629c7f62a00ba

fix: parsing scientific notation in SVG path (#365)
## Before this change

The SVG parsing code failed to parse scientific notation in `BezPath`.
For example `M 0 0 L 1e-123 -4E+5` would fail to parse with
`SvgParseError::Wrong` even though it is a valid SVG path. This error
was introduced by 8d10d4e which [forgot to negate the
conditional](https://github.com/linebender/kurbo/commit/8d10d4e00981238f1ba555fe44af000b81aa4ec1#diff-a0ea1370686e222812265cfdba79ff5c9458cfc267ebd7c8b2221dc735909483L317),
causing the parser to fail when it should succeed.

## After this change

The change fixes the error and adds a test for it.

src/svg.rs
@@ -352,11 +352,11 @@ impl<'a> SvgLexer<'a> {
                 if c == b'-' || c == b'+' {
                     c = self.get_byte().ok_or(SvgParseError::Wrong)?;
                 }
-                if c.is_ascii_digit() {
+                if !c.is_ascii_digit() {
                     return Err(SvgParseError::Wrong);
                 }
                 while let Some(c) = self.get_byte() {
-                    if c.is_ascii_digit() {
+                    if !c.is_ascii_digit() {
                         self.unget();
                         break;
                     }
@@ -506,7 +506,7 @@ impl Arc {
 
 #[cfg(test)]
 mod tests {
-    use crate::{BezPath, CubicBez, Line, ParamCurve, PathSeg, Point, QuadBez, Shape};
+    use crate::{BezPath, CubicBez, Line, ParamCurve, PathEl, PathSeg, Point, QuadBez, Shape};
 
     #[test]
     fn test_parse_svg() {
@@ -541,6 +541,22 @@ mod tests {
     fn test_parse_svg_uninitialized() {
         let path = BezPath::from_svg("L10 10 100 0 0 100");
         assert!(path.is_err());
+    }
+
+    #[test]
+    #[allow(clippy::float_cmp)]
+    fn test_parse_scientific_notation() {
+        let path = BezPath::from_svg("M 0 0 L 1e-123 -4E+5").unwrap();
+        assert_eq!(
+            path.elements(),
+            &[
+                PathEl::MoveTo(Point { x: 0.0, y: 0.0 }),
+                PathEl::LineTo(Point {
+                    x: 1e-123,
+                    y: -4E+5
+                })
+            ]
+        );
     }
 
     #[test]